65e1e7cfe93ee3eb1c3f4d9616386f2db913afc4
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-01-14 10:03:07 +0000

    fix modeltranslation admin feedback

--- geotrek/feedback/admin.py
+++ geotrek/feedback/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from geotrek.feedback import models as feedback_models
 
-from modeltranslation.admin import TranslationAdmin
+if 'modeltranslation' in settings.INSTALLED_APPS:
+    from modeltranslation.admin import TranslationAdmin
+else:
+    TranslationAdmin = admin.ModelAdmin
 
 
 admin.site.register(feedback_models.ReportCategory, TranslationAdmin)
